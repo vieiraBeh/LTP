b3764d62af45fd8bdffd2548c65d6ab5afa8522c
VieiraBeh <brendanascimentovieirah@gmail.com>
2023-06-07 14:21:24 -0400

    commit

--- exercicios_try_catch/exercicio_02.js
+++ exercicios_try_catch/exercicio_02.js
@@ -2,6 +2,13 @@
   Todos os atributos necessários. Assim, usando Try e Catch, trate os erros e apresente uma mensagem quando houve atributos vazios.
 */
 
+class MeuErro extends Error {
+  constructor(message){
+    super(message);
+    this.name = "Meu Erro";
+  }
+}
+
 class Estudante {
   constructor(nome, idade, turma) {
     this.nome = nome;
@@ -10,24 +17,34 @@ class Estudante {
   }
 
   mostrarAtributos() {
-    return this.atributos();
+    try {
+      return this.atributos();
+    } catch (error) {
+      return error
+    }
   }
 
   atributos() {
-    return {
-      nome: this.nome,
-      idade: this.idade,
-      turma: this.turma
-    };
+    if (this.nome != ""){
+      return {
+        nome: this.nome,
+        idade: this.idade,
+        turma: this.turma
+      };
+    } else {
+      throw new MeuErro("Mensagem de erro");
+    }
   }
 }
 
-const estudante = new Estudante("João", 16, "3A");
+const estudante = new Estudante("", 16, "3A");
 
-const atributos = estudante.mostrarAtributos();
+console.log(estudante.mostrarAtributos());
 
-console.log(atributos.nome);   
+/*console.log(atributos.nome);   
 console.log(atributos.idade);  
-console.log(atributos.turma); 
+console.log(atributos.turma); */
+
+
 
 //depois de new é classe, antes objeto
